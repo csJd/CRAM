3a6cf4c33c414ac830f0fefe45dd2fb6c7ebc01f
Deng <d.vay@qq.com>
2019-07-05 12:40:32 +0800

    add predict method in eval.py

--- eval.py
+++ eval.py
@@ -5,9 +5,11 @@ import torch
 from torch.utils.data import DataLoader
 from sklearn.metrics import classification_report
 
-from dataset import End2EndDataset
+from dataset import End2EndDataset, gen_sentence_tensors
 from utils.torch_util import calc_f1
 from utils.path_util import from_project_root
+
+LABELS = ["NA", "DNA", "RNA", "protein", "cell_line", "cell_type"]
 
 
 def evaluate_e2e(model, data_url, bsl_model=None):
@@ -27,12 +29,13 @@ def evaluate_e2e(model, data_url, bsl_model=None):
     loader = DataLoader(dataset, batch_size=200, collate_fn=dataset.collate_func)
     ret = {'precision': 0, 'recall': 0, 'f1': 0}
 
+    sentence_true_list, sentence_pred_list = list(), list()
+    region_true_list, region_pred_list = list(), list()
+    region_true_count, region_pred_count = 0, 0
+
     # switch to eval mode
     model.eval()
     with torch.no_grad():
-        sentence_true_list, sentence_pred_list = list(), list()
-        region_true_list, region_pred_list = list(), list()
-        region_true_count, region_pred_count = 0, 0
         for data, sentence_labels, region_labels, records_list in loader:
             if bsl_model:
                 pred_sentence_labels = torch.argmax(bsl_model.forward(*data), dim=1)
@@ -63,6 +66,10 @@ def evaluate_e2e(model, data_url, bsl_model=None):
                             if pred_region_labels[ind] > 0:
                                 pred_records[(start, end)] = pred_region_labels[ind]
                             ind += 1
+
+                region_true_count += len(true_records)
+                region_pred_count += len(pred_records)
+
                 for region in true_records:
                     true_label = dataset.label_ids[true_records[region]]
                     pred_label = pred_records[region] if region in pred_records else 0
@@ -72,10 +79,6 @@ def evaluate_e2e(model, data_url, bsl_model=None):
                     if region not in true_records:
                         region_pred_list.append(pred_records[region])
                         region_true_list.append(0)
-
-            region_labels = region_labels.view(-1).cpu()
-            region_true_count += int((region_labels > 0).sum())
-            region_pred_count += int((pred_region_labels > 0).sum())
 
             pred_sentence_labels = pred_sentence_labels.view(-1).cpu()
             sentence_labels = sentence_labels.view(-1).cpu()
@@ -93,8 +96,6 @@ def evaluate_e2e(model, data_url, bsl_model=None):
         ret = dict()
         tp = 0
         for pv, tv in zip(region_pred_list, region_true_list):
-            if pv == tv == 0:
-                continue
             if pv == tv:
                 tp += 1
         fp = region_pred_count - tp
@@ -104,11 +105,76 @@ def evaluate_e2e(model, data_url, bsl_model=None):
     return ret
 
 
+def predict(model, sentences):
+    """ predict NER result for sentence list
+    Args:
+        model: trained model
+        sentences: sentences to be predicted
+    Returns:
+        predicted results: ([sentence_labels], [region_labels], lengths)
+    """
+
+    tensors = gen_sentence_tensors(sentences, device=next(model.parameters()).device,
+                                   data_url=from_project_root('data/genia/vocab.json'))
+    pred_region_output, pred_sentence_output = model.forward(*tensors)
+    pred_sentence_labels = torch.argmax(pred_sentence_output, dim=1).cpu()
+    pred_region_labels = torch.argmax(pred_region_output, dim=1).cpu()
+
+    lengths = tensors[1]
+    pred_sentence_records = []
+
+    ind = 0
+    for sent_labels, length in zip(pred_sentence_labels, lengths):
+        pred_records = dict()
+        for start in range(0, length):
+            if sent_labels[start] == 1:
+                for end in range(start + 1, length):
+                    if sent_labels[end - 1] == 0:
+                        break
+                    if pred_region_labels[ind] > 0:
+                        pred_records[(start, end)] = LABELS[pred_region_labels[ind]]
+                    ind += 1
+        pred_sentence_records.append(pred_records)
+
+    return pred_sentence_labels, pred_sentence_records, lengths
+
+
+def predict_on_iob2(model, iob_url):
+    """ predict on iob2 file and save the results
+    Args:
+        model: trained model
+        iob_url: url to iob file
+    """
+
+    save_url = iob_url.replace('.iob2', '.pred.txt')
+    print("predicting on {} \n the result will be saved in {}".format(
+        iob_url, save_url))
+    test_set = End2EndDataset(iob_url, device=next(
+        model.parameters()).device)
+
+    model.eval()
+    with open(save_url, 'w', encoding='utf-8', newline='\n') as save_file:
+        for sentence, records in test_set:
+            save_file.write(' '.join(sentence) + '\n')
+            save_file.write("length = {} \n".format(len(sentence)))
+            save_file.write("Gold records: {}\n".format(str(records)))
+            # print(predict(model, [sentence]))
+            sentence_labels, sentence_records, length =\
+                list(zip(*predict(model, [sentence])))[0]
+            save_file.write("Pred binary labels: {}\n".format(str(sentence_labels)))
+            save_file.write("Pred records: {}\n".format(str(sentence_records)))
+            details = str([sentence[rg[0]:rg[1]] for rg in sentence_records])
+            save_file.write("{}\n\n".format(details))
+
+
 def main():
     model_url = from_project_root("data/model/best_model.pt")
-    test_url = from_project_root("data/genia.test.iob2")
+    print("loading model from", model_url)
     model = torch.load(model_url)
+    # model = torch.load(model_url, map_location='cpu')
+    test_url = from_project_root("data/genia/genia.test.iob2")
     evaluate_e2e(model, test_url)
+    # predict_on_iob2(model, test_url)
     pass
 
 
